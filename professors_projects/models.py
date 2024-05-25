e565a1903a43de42a1c9f871b4ee3b00f09f87f9
Mahdi Khoramabadi <mahdi.sidsid@gmail.com>
2024-05-25 18:29:52 +0330

    updated project id

--- professors_projects/models.py
+++ professors_projects/models.py
@@ -1,5 +1,7 @@
+# models.py
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Max
 
 class Professor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -21,13 +23,12 @@ class Student(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-    
 
 class Project(models.Model):
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    project_id = models.CharField(max_length=4, unique=True)
+    project_id = models.CharField(max_length=4, unique=True, default='1000')  # Temporary default
     max_students = models.PositiveIntegerField(default=4)
     is_available = models.BooleanField(default=True)
     claimed_by = models.ManyToManyField(Student, through='ProjectClaim')
@@ -35,6 +36,15 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.project_id or self.project_id == '1000':  # Check if the project_id is not set or default
+            last_project_id = Project.objects.aggregate(Max('project_id'))['project_id__max']
+            if last_project_id:
+                self.project_id = str(int(last_project_id) + 1)
+            else:
+                self.project_id = '1000'
+        super().save(*args, **kwargs)
 
     def update_availability(self):
         if self.claimed_by.count() >= self.max_students:
